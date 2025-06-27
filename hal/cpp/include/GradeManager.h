92593af68acffe7f1a7c5e00cb2b3307337b0591
Richard Kats <richardkatz@gmx.de>
2025-06-27 14:21:34 +0200

    defined AIDL interface for dummy grade manager

---
+++ hal/cpp/include/GradeManager.h
@@ -0,0 +1,30 @@
+//
+// Created by richard on 26.06.25.
+//
+
+#ifndef AOSP_PLAYGROUND_GRADEMANAGER_H
+#define AOSP_PLAYGROUND_GRADEMANAGER_H
+
+#include <aidl/android/hardware/grademanager/BnGradeManager.h>
+
+namespace aidl {
+    namespace android {
+        namespace hardware {
+            namespace grademanager {
+                class GradeManager: public BnGradeManager {
+                public:
+                    ndk::ScopedAStatus getGrade(int32_t studentIdx, int32_t* out) override;
+                    ndk::ScopedAStatus setGrade(int32_t studentIdx, int32_t grade) override;
+                    ndk::ScopedAStatus clearGradesAndSetGradeListToNull() override;
+                    ndk::ScopedAStatus clearGradesAndFreeGradeList() override;
+                    ndk::ScopedAStatus initializeGradeList() override;
+                private:
+                    int32_t* grades;
+                };
+
+            }
+        }
+    }
+}
+
+#endif //AOSP_PLAYGROUND_GRADEMANAGER_H
