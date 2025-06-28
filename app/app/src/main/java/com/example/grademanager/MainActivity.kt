55e29a49b16d7bfe72690402f883c5cadca03387
Richard Kats <richardkatz@gmx.de>
2025-06-28 20:06:28 +0200

    adjust app and Android.bp

--- app/app/src/main/java/com/example/grademanager/MainActivity.kt
+++ app/app/src/main/java/com/example/grademanager/MainActivity.kt
@@ -4,43 +4,112 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.grademanager.ui.theme.GrademanagerTheme
+
 class MainActivity : ComponentActivity() {
+    private val viewModel = HalViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             GrademanagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GradeManagerScreen(viewModel)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun GradeManagerScreen(viewModel: HalViewModel) {
+    val halStatus by viewModel.halStatus.collectAsState()
+    val resultText by viewModel.resultText.collectAsState()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GrademanagerTheme {
-        Greeting("Android")
+    var studentIdx by remember { mutableStateOf(0) }
+    var grade by remember { mutableStateOf(0) }
+
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = halStatus,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = resultText,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            value = studentIdx,
+            onValueChange = { newStudentIdx ->
+                studentIdx = newStudentIdx
+            },
+            placeholder = { Text("Student ID") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = grade,
+            onValueChange = { newGrade ->
+                grade = newGrade
+            },
+            placeholder = { Text("Grade") }
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Set grade")
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Get grade")
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Set grade list to null")
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Free grade list memory")
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Initialize grade list")
+        }
     }
 }
